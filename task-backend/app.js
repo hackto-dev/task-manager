f5c863bf90aa6801474cbdf489f64436440e784e
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-04-14 19:53:44 +0530

    Added authentication using JWT

--- task-backend/app.js
+++ task-backend/app.js
@@ -6,11 +6,12 @@ const bodyParser = require('body-parser');
 // Load in the mongoose models
 const { List } = require('./db/models/list.model');
 const { Task } = require('./db/models/task.model');
+const { User } = require('./db/models/user.model');
 
-// Load middleware
+/* Middleware */
 app.use(bodyParser.json());
 
-// Added CORS middleware
+// CORS Middleware
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
@@ -18,6 +19,46 @@ app.use(function (req, res, next) {
   
   next();
 });
+
+// Auth Middleware
+let verifySession = (req, res, next) => {
+    let refreshToken = req.header('x-refresh-token');
+
+    let _id = req.header('_id');
+
+    User.findByIdAndToken(_id, refreshToken).then((user) => {
+        if (!user) {
+            // User couldn't be found
+            Promise.reject({
+                'error': 'User not found. Make sure refresh token and user ID are legit'
+            });
+        } else {
+            req.user_id = user._id;
+            req.userObject = user;
+            req.refreshToken = refreshToken;
+
+            let isSessionValid = false;
+
+            user.sessions.forEach((session) => {
+                if (session.token === refreshToken) {
+                    if (User.hasRefreshTokenExpired(session.expiresAt) === false) {
+                        isSessionValid = true;
+                    }
+                }
+            });
+
+            if (isSessionValid) {
+                next();
+            } else {
+                return Promise.reject({
+                    'error': 'Refresh token expired or session invalid'
+                });
+            }
+        }
+    }).catch((e) => {
+        res.status(401).send(e);
+    });
+}
 
 /* Route Handlers */
 
@@ -149,6 +190,77 @@ app.delete('/lists/:listId/tasks/:taskId',(req,res) => {
         res.send(removedTaskDoc);
     });
 });
+
+/* User Routes */
+
+/**
+ * POST /users
+ * Purpose: Sign Up
+ */
+app.post('/users',(req,res) => {
+    // User will sign up
+    let body = req.body;
+
+    let newUser = new User(body);
+
+    newUser.save().then(() => {
+        return newUser.createSession();
+    }).then((refreshToken) => {
+        //Session created successfully - refresh token returned
+        // Now we generate access auth token for the user
+
+        return newUser.generateAccessAuthToken().then((accessToken) => {
+            return { accessToken,refreshToken };
+        })
+    }).then((authTokens) => {
+        res
+            .header('x-refresh-token',authTokens.refreshToken)
+            .header('x-access-token',authTokens.accessToken)
+            .send(newUser);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+/**
+ * POST /users/login
+ * Purpose: Login
+ */
+app.post('/users/login',(req,res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findByCredentials(email,password).then((user) => {
+        return user.createSession().then((refreshToken) => {
+            return user.generateAccessAuthToken().then((accessToken) => {
+                return { accessToken,refreshToken };
+            });
+        }).then((authTokens) => {
+            res
+                .header('x-refresh-token', authTokens.refreshToken)
+                .header('x-access-token', authTokens.accessToken)
+                .send(user);
+        }).catch((e) => {
+            res.status(400).send(e);
+        });
+    });
+})
+
+/**
+ * GET /users/me/access-token
+ * Purpose: Generates and returns an access token
+ */
+app.get('/users/me/access-token', verifySession, (req, res) => {
+    // we know that the user/caller is authenticated and we have the user_id and user object available to us
+    req.userObject.generateAccessAuthToken().then((accessToken) => {
+        res.header('x-access-token', accessToken).send({
+            accessToken
+        });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
+
 
 app.listen(5000,() => {
     console.log("Server listening on port 3000");
